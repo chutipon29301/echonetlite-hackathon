7dcc31a60c59cc825db893467f6d493bbd1a3274
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:23:11 +0700

    feat(back->discovery): add discovery router

--- backend/src/discovery/discovery.controller.ts
+++ backend/src/discovery/discovery.controller.ts
@@ -6,4 +6,14 @@ import { EchonetDevice } from '../types';
 export class DiscoveryController {
 
     constructor(private readonly discoveryService: DiscoveryService) { }
+
+    @Get()
+    public list(): { devices: EchonetDevice[] } {
+        return { devices: this.discoveryService.getCurrentDevice() };
+    }
+
+    @Get('info')
+    public listIpAddress(): { addresses: string[] } {
+        return { addresses: this.discoveryService.getCurrentAddress() };
+    }
 }
