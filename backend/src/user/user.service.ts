240ff024905d1b9d51ee81f539d36a5c9159cfc5
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:25:09 +0700

    feat(back->user): add user service

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CalculationService } from '../calculation/calculation.service';
 
 @Injectable()
 export class UserService {
+
+    constructor(private readonly calculationService: CalculationService) { }
+
+    public setBedtime(date: Date, temp: number) {
+        this.calculationService.createOpenJob(date, temp);
+    }
+
+    public setBedtimeFor(date: Date, temp: number, ipAddress: string) {
+        this.calculationService.createOpenJobFor(date, temp, ipAddress);
+    }
 }
 
