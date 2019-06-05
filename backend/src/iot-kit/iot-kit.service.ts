469ae8ef20fb1d40bc2a5e773d3c3fab99176749
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:24:01 +0700

    feat(back->iot): add iot service

--- backend/src/iot-kit/iot-kit.service.ts
+++ backend/src/iot-kit/iot-kit.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { IotDevice } from '../types';
 
 @Injectable()
 export class IotKitService {
+
+    private iotDevice: IotDevice = { humidity: 0, temperature: 0, light: 0 };
+
+    public iotDataReceiver(humidity: number, temperature: number, light: number) {
+        this.iotDevice.humidity = humidity;
+        this.iotDevice.temperature = temperature;
+        this.iotDevice.light = light;
+    }
+
+    public getIotHumidity() {
+        return this.iotDevice.humidity;
+    }
+    public getIotTemperature() {
+        return this.iotDevice.temperature;
+    }
+    public getIotLight() {
+        return this.iotDevice.light;
+    }
 }
 
