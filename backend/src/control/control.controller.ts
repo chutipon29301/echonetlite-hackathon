ed8968f2752b0a5d3fc41a3920df5bf63257f68f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:21:06 +0700

    feat(back->control): add control router

--- backend/src/control/control.controller.ts
+++ backend/src/control/control.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { AirTriggerDto, ActionType } from './dto/air-trigger.dto';
 import { ControlService } from './control.service';
 
 @Controller('control')
 export class ControlController {
     constructor(private readonly controlService: ControlService) {}
+
+    @Post()
+    public airTrigger(@Body() body: AirTriggerDto) {
+        return this.controlService.trigger(body.ipAddress, body.airStatus);
+    }
 
 }
