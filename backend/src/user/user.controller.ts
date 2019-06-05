4a193196695869effd457cb56600f8a9e8df46fd
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:24:58 +0700

    feat(back->user): add user router

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -5,5 +5,17 @@ import { UserService } from './user.service';
 export class UserController {
 
     constructor(private readonly userService: UserService) { }
+
+    @Post('setBedTime')
+    public setBedTime(@Body('date') dateString: string, @Body('temp') temp: number) {
+        const date = new Date(dateString);
+        this.userService.setBedtime(date, temp);
+    }
+
+    @Post('setBedTimeFor')
+    public setBedTimeFor(@Body('date') dateString: string, @Body('temp') temp: number, @Body('ipAddress') ipAddress: string) {
+        const date = new Date(dateString);
+        this.userService.setBedtimeFor(date, temp, ipAddress);
+    }
 }
 
