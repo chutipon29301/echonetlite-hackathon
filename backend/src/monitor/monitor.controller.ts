1a54e6f26bd87d1ebf84f5708de562b182abbbe6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:24:12 +0700

    feat(back->monitor): add monitor controller

--- backend/src/monitor/monitor.controller.ts
+++ backend/src/monitor/monitor.controller.ts
@@ -1,6 +1,27 @@
 import { Controller, Get, Query, Param } from '@nestjs/common';
+import { MonitorService, EchonetDeviceInfo, EchonetDeviceDetail } from './monitor.service';
+import { EchonetClass } from '../constants';
 
 @Controller('monitor')
 export class MonitorController {
+
+    constructor(private readonly monitorService: MonitorService) { }
+
+    @Get()
+    public async detail(@Query('ipAddress') ipAddress: string, @Query('echonetClass') echonetClass: string) {
+        return await this.monitorService.getEchonetClassFromDeviceWithIpAddress(ipAddress, EchonetClass[echonetClass]);
+    }
+
+    @Get('allDevice')
+    public async allDeviceSummary(): Promise<{ devices: EchonetDeviceInfo[] }> {
+        const devices = await this.monitorService.listEchonetDeviceInfo();
+        return { devices };
+    }
+
+    @Get('detail/:ipAddress')
+    public async deviceDetailByIp(@Param('ipAddress') ipAddress: string): Promise<{ detail: EchonetDeviceDetail }> {
+        const detail = await this.monitorService.getEchonetDeviceDetail(ipAddress);
+        return {detail};
+    }
 }
 
