daf2b0486c33748f09c49b6afe790882d784ce05
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:24:46 +0700

    feat(back->search): add search service

--- backend/src/search/search.module.ts
+++ backend/src/search/search.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+
 @Module({
+  imports: [ElasticsearchModule.register({
+    host: 'localhost:9200',
+    log: 'trace',
+  })],
   providers: [SearchService],
 })
 export class SearchModule { }
--- backend/src/search/search.service.ts
+++ backend/src/search/search.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { IotDataDto } from '../iot-kit/dto/iot-data.dto';
 
 @Injectable()
 export class SearchService {
+    constructor(private readonly elasticsearchService: ElasticsearchService) { }
+    public saveIotElastic(iotKitData: IotDataDto) {
+        this.elasticsearchService.bulk({
+            body: [
+              // action description
+              { index:  { _index: new Date(), _type: 'mytype', _id: 1 } },
+               // the document to index
+              { title: 'foo' },
+              // action description
+              { update: { _index: 'myindex', _type: 'mytype', _id: 2 } },
+              // the document to update
+              { doc: { title: 'foo' } },
+            ],
+          });
+    }
+    // public saveEchonetElastic(iotKitData: IotDataDto) {
+
+    // }
 }
 
