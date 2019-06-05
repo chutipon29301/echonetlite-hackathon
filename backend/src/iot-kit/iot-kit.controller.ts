05af01007c107f8b8ac137501865be76cc8946db
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:23:45 +0700

    feat(back->iot): add iot router

--- backend/src/iot-kit/iot-kit.controller.ts
+++ backend/src/iot-kit/iot-kit.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { IotKitService } from './iot-kit.service';
+import { IotDataDto } from './dto/iot-data.dto';
+
 @Controller('iot-kit')
 export class IotKitController {
 
     constructor(private readonly iotKitService: IotKitService) { }
 
+    @Post()
+    public getIoTData(@Body() body: IotDataDto) {
+        this.iotKitService.iotDataReceiver(body.humidity, body.temperature, body.light);
+    }
 }
 
