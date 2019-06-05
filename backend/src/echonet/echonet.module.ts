dac8f6e86724dd9b4199eed2344c7a6bf1d3d507
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:07:38 +0700

    feat(back->echonet): add echonet module

---
+++ backend/src/echonet/echonet.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { echonetProviders } from './echonet.providers';
+
+@Module({
+  providers: [...echonetProviders],
+  exports: [...echonetProviders],
+})
+export class EchonetModule { }
---
+++ backend/src/echonet/echonet.providers.ts
@@ -0,0 +1,12 @@
+import { Provider } from '@nestjs/common';
+import { EchonetConnectionToken } from '../constants';
+import * as  EchonetLite from 'node-echonet-lite';
+import { Echonet } from '../types';
+
+export const echonetProviders: Provider[] = [{
+    provide: EchonetConnectionToken,
+    useFactory: async () => await new Promise<Echonet>((resolve, reject) => {
+        const echonetLite = new EchonetLite({ type: 'lan' });
+        resolve(echonetLite);
+    }),
+}];
