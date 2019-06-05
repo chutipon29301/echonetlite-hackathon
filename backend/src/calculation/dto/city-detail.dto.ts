c66018608e4b3c527448be77039a83b06f0900e0
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-05 10:20:03 +0700

    feat(back->calculation): create calculation related dto

---
+++ backend/src/calculation/dto/city-detail.dto.ts
@@ -0,0 +1,3 @@
+export class CityDetailDto {
+    sunrise: number;
+}
---
+++ backend/src/calculation/dto/weather-response-request.dto.ts
@@ -0,0 +1,5 @@
+import { CityDetailDto } from './city-detail.dto';
+
+export class WeatherRequestResponse {
+    sys: CityDetailDto;
+}
